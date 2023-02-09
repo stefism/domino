1f55d58241289e4f9b344773ff68fc0f5b5e3af9
Stefan Markov <stefan.t.markov@gmail.com>
2023-02-09 17:22:27 +0200

    Add header and footer and some images

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <footer>
+    <div class="container">
+        <div>
+            <img src="@/assets/images/logo.png" alt="" />
+            <div class="contact">
+                <div>Call Us</div>
+                <div>+359 879 123 123</div>
+            </div>
+        </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <header>
+    <div class="container clear">
+      <div class="logo">
+        <img :src="logoIcon" alt="" />
+      </div>
+      <div class="nav">
+        <img :src="menuIcon" alt="" />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import Logo from "@/assets/images/logo.png";
+import Menu from "@/assets/images/menu.png";
+
+export default {
+  data() {
+    return {
+      logoIcon: Logo,
+      menuIcon: Menu,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
